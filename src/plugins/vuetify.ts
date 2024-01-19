896cdb8abfbcf64704bebd6e04a518350133d1f0
Habibi Coding <habibicoding@aol.com>
2024-01-19 23:46:15 +0100

    chore: Adapt general project set up.

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -9,9 +9,18 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 // Composables
-import { createVuetify } from 'vuetify'
+import {createVuetify} from 'vuetify'
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
-  //
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: '#145201', // dark green
+          secondary: '#5cf680' // light green
+        }
+      }
+    }
+  }
 })
