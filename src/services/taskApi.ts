0ab80670d827402e3dc5b1bbc18d235b1f69d701
Habibi Coding <habibicoding@aol.com>
2024-01-20 19:08:50 +0100

    fix: Fix nesting statements.

--- src/services/taskApi.ts
+++ src/services/taskApi.ts
@@ -4,6 +4,7 @@ import {HTTP_STATUS_CODES} from "@/constants/appConstants";
 const baseUrl = 'https://backend4vue.onrender.com/api/v1/';
 const api: AxiosInstance = axios.create({baseUrl});
 
+
 api.interceptors.response.use(
   (response: AxiosResponse) => response,
   (error: AxiosError) => {
@@ -13,11 +14,10 @@ api.interceptors.response.use(
         console.error('Bad Request: ', error.response.data);
       } else if (status === HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR) {
         console.error('Internal Server Error: ', error.response.data);
-      } else {
-        console.error('Error: ', error.message);
       }
+    } else {
+      console.error('Error: ', error.message);
     }
     return Promise.reject(error);
   }
 );
-
