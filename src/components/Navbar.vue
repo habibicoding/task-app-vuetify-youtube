def66065cb1108b729af0931f646ce9b18a9a9dc
Habibi Coding <habibicoding@aol.com>
2024-01-20 22:46:43 +0100

    chore: Finish part eight.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -34,9 +34,62 @@ const createTask = () => {
 </script>
 
 <template>
+  <v-app-bar flat>
+    <!-- Container for logo and menu items -->
+    <v-container class="d-flex align-center justify-center">
+      <!-- Logo -->
+      <v-app-bar-title>
+        <v-img src="../assets/logo.png" max-height="70" max-width="70" @click="logoClicked"></v-img>
+      </v-app-bar-title>
 
+      <!-- Menu items for desktop view -->
+      <template v-if="!isMobileDevice.value">
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          @click="selectedTaskType(link)"
+          :text="link"
+          variant="text"
+          :class="{ 'selected-link': link === selectedLink }">
+          {{ link }}
+        </v-btn>
+
+        <v-spacer/>
+
+        <v-btn
+          class="text-none text-subtitle-1"
+          color="#05B990"
+          size="small"
+          variant="outlined"
+          @click="createTask">
+          Create Task
+        </v-btn>
+      </template>
+
+      <!-- Hamburger icon for mobile view -->
+      <v-app-bar-nav-icon v-if="isMobileDevice.value" @click="isDrawerActive = !isDrawerActive"></v-app-bar-nav-icon>
+    </v-container>
+  </v-app-bar>
+
+  <!-- Navigation isDrawerActive for mobile view -->
+  <v-navigation-drawer v-model="isDrawerActive" temporary v-if="isMobileDevice.value">
+    <v-list>
+      <v-list-item
+        v-for="link in links"
+        :key="link"
+        @click="selectedTaskType(link)"
+        :class="{ 'selected-link': link === selectedLink }">
+        <v-list-item-title>{{ link }}</v-list-item-title>
+      </v-list-item>
+      <v-list-item @click="createTask">
+        <v-list-item-title>Create Task</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
 </template>
 
 <style scoped>
-
+.selected-link {
+  color: green !important;
+}
 </style>
